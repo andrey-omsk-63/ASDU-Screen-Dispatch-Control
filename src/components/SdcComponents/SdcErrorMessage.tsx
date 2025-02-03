a90fd2268e046c8a1eff71ccaeb58fc9188e738f
andrey-omsk-63 <andrey_omsk@mail.ru>
2025-02-03 10:30:50 +0600

    0302/1030

--- src/components/SdcComponents/SdcErrorMessage.tsx
+++ src/components/SdcComponents/SdcErrorMessage.tsx
@@ -1,38 +1,38 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
-
-//import { styleModalEnd } from '../MainMapStyle';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
 
 const SdcErrorMessage = (props: { sErr: string; setOpen: any }) => {
   const [openSet, setOpenSet] = React.useState(true);
 
   const styleSetInf = {
-    outline: 'none',
-    position: 'absolute',
-    marginTop: '15vh',
-    marginLeft: '24vh',
+    outline: "none",
+    position: "absolute",
+    marginTop: "15vh",
+    marginLeft: "24vh",
     width: 380,
-    bgcolor: 'background.paper',
-    border: '1px solid #fff',
+    //bgcolor: 'background.paper', // белый
+    bgcolor: "#FFDB4D", // жёлтый
+    border: "1px solid #FFEDA6",
     borderRadius: 1,
+    color: "black",
+    textShadow: "1px 1px 2px rgba(0,0,0,0.3)",
     boxShadow: 24,
-    textShadow: "2px 2px 3px rgba(0,0,0,0.3)",
     p: 1.5,
   };
 
   const styleModalEnd = {
-    position: 'absolute',
-    top: '0%',
-    left: 'auto',
-    right: '-0%',
-    height: '21px',
-    maxWidth: '2%',
-    minWidth: '2%',
-    color: 'red',
+    position: "absolute",
+    top: "0%",
+    left: "auto",
+    right: "-0%",
+    height: "21px",
+    maxWidth: "2%",
+    minWidth: "2%",
+    color: "black",
   };
 
   const handleClose = () => {
@@ -46,7 +46,7 @@ const SdcErrorMessage = (props: { sErr: string; setOpen: any }) => {
         <Button sx={styleModalEnd} onClick={handleClose}>
           <b>&#10006;</b>
         </Button>
-        <Typography variant="h6" sx={{ textAlign: 'center', color: 'red' }}>
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
           {props.sErr}
         </Typography>
       </Box>
